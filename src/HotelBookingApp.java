0b15e9f0ff67848aee41f62063d67d35568137f6
Niyukti-d26 <npjj26@gmail.com>
2026-03-05 10:09:17 +0530

    UC1:Add welcome message

--- src/HotelBookingApp.java
+++ src/HotelBookingApp.java
@@ -1,2 +1,6 @@
-public class HotelBookingApp {
+import java.util.Scanner;
+public static void main(String[] args){
+    System.out.println("Welcome to the Hotel Booking Management System");
+    System.out.println("System initialized successfully");
+    Scanner sc = new Scanner(System.in);
 }
