381c2f07ce2202e6bff1c8c05aa518953f1ddf1d
Niyukti-d26 <npjj26@gmail.com>
2026-03-05 10:32:38 +0530

    UC2:Basic rooms and availability

--- src/HotelBookingApp.java
+++ src/HotelBookingApp.java
@@ -1,6 +1,54 @@
-import java.util.Scanner;
-public static void main(String[] args){
-    System.out.println("Welcome to the Hotel Booking Management System");
-    System.out.println("System initialized successfully");
-    Scanner sc = new Scanner(System.in);
+public class HotelBookingApp {
+
+    public static void main(String[] args) {
+
+        Room single = new SingleRoom();
+        Room doubleRoom = new DoubleRoom();
+        Room suite = new SuiteRoom();
+
+        System.out.println("Single Room Details:");
+        single.displayRoomDetails();
+
+        System.out.println("\nDouble Room Details:");
+        doubleRoom.displayRoomDetails();
+
+        System.out.println("\nSuite Room Details:");
+        suite.displayRoomDetails();
+    }
+}
+
+abstract class Room {
+    protected int numberOfBeds;
+    protected int squareFeet;
+    protected int pricePerNight;
+
+    public Room(int numberOfBeds, int squareFeet, int pricePerNight) {
+        this.numberOfBeds = numberOfBeds;
+        this.squareFeet = squareFeet;
+        this.pricePerNight = pricePerNight;
+    }
+
+    public void displayRoomDetails() {
+        System.out.println("Beds: " + numberOfBeds);
+        System.out.println("Size: " + squareFeet + " sq.ft");
+        System.out.println("Price per night: " + pricePerNight);
+    }
+}
+
+class SingleRoom extends Room {
+    public SingleRoom() {
+        super(1, 250, 1500);
+    }
+}
+
+class DoubleRoom extends Room {
+    public DoubleRoom() {
+        super(2, 400, 2500);
+    }
+}
+
+class SuiteRoom extends Room {
+    public SuiteRoom() {
+        super(3, 750, 5000);
+    }
 }
